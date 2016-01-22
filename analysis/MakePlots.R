ce4174de9da46ec9ed4d5ffab3bae0939fc99e41
Daniel Bennett <danielbrianbennett@gmail.com>
2016-01-22 14:40:01 +1100

    Prettier plots

--- analysis/MakePlots.R
+++ analysis/MakePlots.R
@@ -1,5 +1,5 @@
 # retrieve only data within 7 trials either side of a change
-proximal.data <- subset(sorted.data, sorted.data$changeLag < 8 & sorted.data$changeLag > -8)
+proximal.data <- subset(sorted.data, sorted.data$changeLag < 8 & sorted.data$changeLag > -6)
 
 # aggregate choice proportions by lag number across participants
 choice.by.lag <- aggregate(proximal.data$filledChosen, by = list(proximal.data$changeLag, proximal.data$ID), FUN = mean)
@@ -16,6 +16,13 @@ choice.prop.chart <- ggplot(choice.prop,
                                         colour = "gray",fill = "gray", alpha = 0.7) +
                             geom_line(size = 2) +
                             geom_point(size = 4, shape = 21, fill = "white") +
-                            xlim(-7,7) +
-                            ylim(0,1)
+                            xlim(-5,7) +
+                            ylim(0,1) +
+                            labs(x = "Change Lag", y = "Oddball Choice Proportion", title = "Oddball Choice Proportion Pre/Post Change") +
+                            theme(axis.text = element_text(size = 14), plot.title = element_text(size = 20), axis.title = element_text(size = 16, face = "bold")) + 
+                            scale_x_continuous(breaks = c(-5:-1, 1:7))
+                              
+
+# build plot
+choice.prop.chart
                             
