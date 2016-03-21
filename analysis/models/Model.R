2b017c40b8ed606d43ef571f494295ce26296671
Daniel Bennett <danielbrianbennett@gmail.com>
2016-03-22 08:51:00 +1100

    Added models
    Model.R and PlotOddballBonus.R

---
+++ analysis/models/Model.R
@@ -0,0 +1,35 @@
+# install packages
+library('mvtnorm')
+library('stats4')
+
+# load data
+load("C:/Users/dbennett1/Google Drive/Works in Progress/JSBANDIT/Bandit/data/v2.2/banditData_v2point2.RData")
+
+# get input parameters
+nBlocks <- 3
+nBandits <- 4
+nTrials <- 30
+varianceGamma_start <- 4
+varianceEpsilon_start <- 4
+participantID <- 10868001
+
+source("C:/Users/dbennett1/Documents/GitHub/jsbandit/analysis/KalmanPMU.R")
+
+LL <- function(gamma,epsilon) {
+  output <- KalmanPMU(sorted.data,participantID,gamma,epsilon)
+  return(output$negativeLL)
+}
+
+t1 <- Sys.time()
+mle.fit <- mle(LL, 
+               start = list(gamma = varianceGamma_start),
+               fixed = list(epsilon = 4))
+t2 <- Sys.time()
+
+difftime(t2,t1)
+
+output <- KalmanPMU(sorted.data,participantID,mle.fit@coef[1],4)
+# plot bandit means
+for (i in 1:nBlocks) {
+  matplot(t(output$banditMeans[,,i]), type = "b")
+}
---
+++ analysis/models/PlotOddballBonus.R
@@ -0,0 +1,26 @@
+# parameters
+A <- 6 # size of change bonus
+k <- 2 # trial decay parameter
+j <- 1 # block decay parameter
+
+# constants
+nTrials <- 30 # number of trials to calculate bonus size for
+nTrialsToPlot <- 15 # number of trials to plot bonus size for
+nBlocks <- 3
+
+# function
+bonusFunction <- function(nTrials, blockNumber, changeBonus, trialDecay, blockDecay){
+  
+  return( changeBonus * ( blockNumber ^ -blockDecay ) * ( c(1:nTrials) ^ -trialDecay ) )
+}
+
+# calculate and plot bonuses as a function of block and trial number
+bonuses <- matrix(nrow = nBlocks, ncol = nTrials)
+par(mfrow = c(1,nBlocks))
+
+for (i in 1:nBlocks){
+  
+  bonuses[i,1:nTrials] <- bonusFunction(nTrials, i, A, k, j)
+  plot(1:nTrialsToPlot, bonuses[i,1:nTrialsToPlot], type = "b", ylim = c(1,min(100,max(30, A * 2.5))),main = sprintf("Block %.0f", i), xlab = "N Trials Post-Change", ylab = "Oddball Bonus")
+  
+}
