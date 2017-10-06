eec9b07815fad2309583c56a6a232ad74787eb63
Daniel Bennett <daniel.bennett@princeton.edu>
2017-10-06 16:04:34 -0400

    Update plots

--- analysis/CompareAlgorithms.R
+++ analysis/CompareAlgorithms.R
@@ -59,29 +59,62 @@ for (i in 1:nParticipants){
 }
  
 meanWinnings_random <- rowMeans(colMeans(winnings_random))
-stdErrWinnings_random <- 1.96 * apply(colMeans(winnings_random),c(1), sd) / sqrt(nParticipants)
+ciWinnings_random <- 1.96 * apply(colMeans(winnings_random),c(1), sd) / sqrt(nParticipants)
 meanWinnings_perfect <- rowMeans(colMeans(winnings_perfect))
-stdErrWinnings_perfect <- 1.96 *apply(colMeans(winnings_perfect),c(1), sd) / sqrt(nParticipants)
+ciWinnings_perfect <- 1.96 *apply(colMeans(winnings_perfect),c(1), sd) / sqrt(nParticipants)
 meanWinnings <- rowMeans(colMeans(winnings))
-stdErrWinnings <- 1.96 *apply(colMeans(winnings),c(1), sd) / sqrt(nParticipants)
+ciWinnings <- 1.96 *apply(colMeans(winnings),c(1), sd) / sqrt(nParticipants)
 
-## make plot
+## make plot 1
 
 df <- data.frame(meanPoints = c(meanWinnings_random, meanWinnings, meanWinnings_perfect), 
-                 stdErrPoints = c(stdErrWinnings_random, stdErrWinnings, stdErrWinnings_perfect),
-                 algorithm = c(rep("random",30), rep("behav", 30), rep("perfect", 30)),
+                 ciPoints = c(ciWinnings_random, ciWinnings, ciWinnings_perfect),
+                 algorithm = c(rep("Random",30), rep("Behaviour", 30), rep("Maximum", 30)),
                  trialNo = rep(1:30,3))
 
-cols <- c("random" = "#bfbfbf", "behav" = "#235ce1", "perfect" = "#000000")
+cols <- c("Random" = "#000000", "Behaviour" = "#235ce1", "Maximum" = "#000000")
 
 p <- ggplot(data = df, aes(x = trialNo, y = meanPoints,  group = algorithm)) +
-  geom_ribbon(aes(ymin = meanPoints - stdErrPoints, ymax = meanPoints + stdErrPoints), 
-              fill = "grey70", alpha = 0.3)  +
-  geom_line(aes(color = algorithm, linetype = algorithm)) +
-  geom_point(aes(color = algorithm, shape = algorithm))
+  geom_ribbon(aes(ymin = meanPoints - ciPoints, ymax = meanPoints + ciPoints), 
+              fill = "grey", alpha = 0.6)  +
+  geom_line(aes(color = algorithm, linetype = algorithm), size = 1) +
+  geom_point(aes(color = algorithm, shape = algorithm),size = 3, fill = "#235ce1") +
+  labs(x = "\nTrial number", y = "Points per trial\n")
      
-p + scale_color_manual(values = cols) + ylim(0,90) + xlim(0.5, 30.5) +
-  scale_linetype_manual(values = c("solid","dashed","dashed")) + 
-  scale_shape_manual(values = c(1,0,0)) +
-  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), axis.line = element_line(color = "black", size = 0.3)) +
-  scale_x_continuous(breaks = c(5,10,15,20,25,30))
+p + scale_color_manual(values = cols) +
+  scale_linetype_manual(values = c("solid","solid","dotted")) + 
+  scale_shape_manual(values = c(21,32,32)) +
+  theme(panel.grid = element_blank(), 
+        panel.background = element_rect(fill = "white"), 
+        axis.line = element_line(color = "black", size = 0.3),
+        axis.title = element_text(face = "bold", size = 18),
+        axis.text = element_text (size = 16),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16)) +
+  scale_x_continuous(breaks = c(5,10,15,20,25,30), expand = c(0,0), limits = c(0,30)) +
+  scale_y_continuous(breaks = c(30, 40, 50, 60, 70, 80), expand = c(0,0), limits = c(30,90))
+
+
+## make plot 2
+
+blockMeans <- apply(winnings,c(1,3),mean)
+overallBlockMean <- apply(blockMeans,1,mean)
+overallBlockCI <- 1.96 * apply(blockMeans,1,sd) / sqrt(nParticipants)
+
+df <- data.frame(meanPoints = overallBlockMean, 
+                 ciPoints = overallBlockCI,
+                 block = c("Block 1","Block 2","Block 3"))
+
+p <- ggplot(data = df, aes(x = block, y = meanPoints)) +
+  geom_col(width = 0.6, fill = "white",colour = "black", size = 1.5) +
+  geom_errorbar(aes(y = meanPoints, ymin = meanPoints - ciPoints, ymax = meanPoints + ciPoints, width = 0.2)) +
+  labs(x = "\nBlock", y = "Points per trial\n")
+
+
+p + theme(panel.grid = element_blank(), 
+        panel.background = element_rect(fill = "white"),
+        axis.line = element_line(color = "black", size = 0.3),
+        axis.title = element_text(face = "bold", size = 18),
+        axis.text = element_text (size = 16)) +
+  scale_y_continuous(breaks = c(30, 40, 50, 60, 70, 80), expand = c(0,0)) +
+  coord_cartesian(ylim = c(30,90))
