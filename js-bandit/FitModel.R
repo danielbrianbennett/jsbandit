3c582071094d209ba12a97d3c9882977020057cb
Daniel Bennett <daniel.bennett@princeton.edu>
2018-04-04 16:57:56 -0400

    Add parameter estimation via importance sampling

--- js-bandit/FitModel.R
+++ js-bandit/FitModel.R
@@ -1,11 +1,15 @@
-library(mvtnorm)
+#### Preliminaries ####
+
+require(mvtnorm)
+require(stats4)
+require(tictoc)
 
 source(here::here("helper","HelperFunctions.R"))
 source(here::here("models","ModelLikelihood.R"))
 
 A <- CalculateA()
 allData <- ExtractData(here::here("raw_data","banditData_v2point2.Rdata"))
-pID <- 3
+pID <- 4
 
 data <- list("block" = allData$block[pID,],
              "trial" = allData$trial[pID,],
@@ -14,21 +18,64 @@ data <- list("block" = allData$block[pID,],
              "choice" = allData$choice[pID,],
              "changeLag" = allData$changeLag[pID,])
 
-model <- list()
-model$pars$zeta <- 4
-model$pars$epsilon <- 10
-model$pars$V_init <- 0
-model$pars$var_init <- 100
-model$pars$B <- 10
-model$pars$j <- -0.3
-model$pars$k <- -0.3
 
-LL <- likelihood(data,model)
-print(LL)
 
+#### Specify model ####
+V_init_constant <- 0
+var_init_constant <- 1000
+
+model_KS <- list("pars" = list("zeta" = NA,
+                               "epsilon" = NA,
+                               "B" = NA,
+                               "j" = NA,
+                               "k" = NA),
+                 "lowerBound" = c(0,0,-100,-Inf,-Inf),
+                 "upperBound" = c(100,100,100,Inf,Inf),
+                 "lambda" = function(zeta,epsilon,B,j,k,V_init,var_init){
+                     model$pars$zeta <- zeta
+                     model$pars$epsilon <- epsilon
+                     model$pars$B <- B
+                     model$pars$j <- j
+                     model$pars$k <- k
+                     model$pars$V_init <- V_init
+                     model$pars$var_init <- var_init
+                     ll <- likelihood(fitData,model)
+                     return(ll$negLL)
+                 },
+                 "startPoints" = function(nStarts = 1){
+                     return(list("zeta" = runif(nStarts, min = 0, max = 10),
+                                 "epsilon" = runif(nStarts, min = 0, max = 10),
+                                 "B" = rnorm(nStarts, mean = 0, sd = 20),
+                                 "j" = rnorm(nStarts, mean = 0, sd = 1),
+                                 "k" = rnorm(nStarts, mean = 0, sd = 1)))},
+                 "fixed" = list("V_init" = V_init_constant,
+                                "var_init" = var_init_constant)
+)
+
+model <- model_KS
+fitData <- data
+
+# fit model
+tic()
+fit <- mle(model$lambda,
+           method = "L-BFGS-B",
+           start = model$startPoints(),
+           lower = model$lowerBound,
+           upper = model$upperBound,
+           fixed = model$fixed)
+toc()
+
+# assign parameters to model for testing
+for (n in 1:length(names(fit@fullcoef))){
+    model$pars[names(fit@fullcoef)[n]] <- fit@fullcoef[names(fit@fullcoef)[n]]
+}
+fitLL <- likelihood(data,model)
+
+
+B <- seq(from = -100, to = 100, length.out = 200)
 LL <- NULL
 for(i in 1:length(B)){
     model$pars$B <- B[i]
     LL[i] <- likelihood(data,model)
 }
-plot(exp(-unlist(LL)/90))
+plot(B,exp(-unlist(LL)/90))
--- js-bandit/models/ModelLikelihood.R
+++ js-bandit/models/ModelLikelihood.R
@@ -2,49 +2,61 @@ likelihood <- function(data,model){
     
     # set constants
     nBandits <- 4
+    realMin <- 1e-50
     
     # create containers
     allChoiceProb <- matrix(data = NA, nrow = nBandits, ncol = length(data$block))
-    choiceProb <- rep(NA, times = length(data$block))        
+    choiceProb <- rep(realMin, times = length(data$block))        
     
-    # calculate bonus amount
-    bonus <- CalculateBonus(data$block,data$changeLag,model$pars$B,model$pars$k,model$pars$j)
-    
-    for (i in 1:length(data$trial)){
+    tryCatch({
+
+        # calculate bonus amount
+        bonus <- CalculateBonus(data$block,data$changeLag,model$pars$B,model$pars$k,model$pars$j)
         
-        # re-initialise means on first trial of block
-        if (data$trial[i] == 1){
-            banditMean <- rep(model$pars$V_init, times = nBandits)
-            banditVariance <- rep(model$pars$var_init, times = nBandits)
+        for (i in 1:length(data$trial)){
+            
+            # re-initialise means on first trial of block
+            if (data$trial[i] == 1){
+                banditMean <- rep(model$pars$V_init, times = nBandits)
+                banditVariance <- rep(model$pars$var_init, times = nBandits)
+            }
+            
+            # calculate choice probabilities
+            for (j in 1:nBandits){
+                M = A[,,j] %*% banditMean
+                H = A[,,j] %*% diag(banditVariance + (model$pars$epsilon^2)) %*% t(A[,,j])
+                allChoiceProb[j,i] <- pmvnorm(lower= 0,mean = as.vector(M), sigma = H, algorithm = TVPACK())[1]
+            }
+            choiceProb[i] <- allChoiceProb[data$choice[i],i]
+            
+            # index chosen option and changed option
+            chosenIndicator <- 1 * (1:nBandits == data$choice[i])
+            changedIndicator <- 1 * (1:nBandits == data$whichFilled[i])
+            
+            # update kalman gain
+            kalmanGain <- (banditVariance + model$pars$zeta^2) / (banditVariance + model$pars$zeta^2 + model$pars$epsilon^2)
+            
+            # update bandit means
+            banditMean <- banditMean + (chosenIndicator * kalmanGain * (data$outcome[i] - banditMean))
+            
+            # append bonus to bandit mean
+            if (!(is.na(bonus[i]))){
+                banditMean <- banditMean + bonus[i]
+            }
+            
+            # update bandit variance
+            banditVariance <- (1 - (chosenIndicator * kalmanGain)) * (banditVariance + (model$pars$zeta^2))
+            
         }
         
-        # calculate choice probabilities
-        for (j in 1:nBandits){
-            M = A[,,j] %*% banditMean
-            H = A[,,j] %*% diag(banditVariance + (model$pars$epsilon^2)) %*% t(A[,,j])
-            allChoiceProb[j,i] <- pmvnorm(lower= 0,mean = as.vector(M), sigma = H, algorithm = TVPACK())[1]
-        }
-        choiceProb[i] <- allChoiceProb[data$choice[i],i]
-        
-        # index chosen option and changed option
-        chosenIndicator <- 1 * (1:nBandits == data$choice[i])
-        changedIndicator <- 1 * (1:nBandits == data$whichFilled[i])
-        
-        # update kalman gain
-        kalmanGain <- (banditVariance + model$pars$zeta^2) / (banditVariance + model$pars$zeta^2 + model$pars$epsilon^2)
-        
-        # update bandit means
-        banditMean <- banditMean + (chosenIndicator * kalmanGain * (data$outcome[i] - banditMean))
-        
-        # append bonus to bandit mean
-        if (!(is.na(bonus[i]))){
-            banditMean <- banditMean + bonus[i]
+        if(any(!is.finite(choiceProb))){
+            choiceProb[!is.finite(choiceProb)] <- realMin
         }
         
-        # update bandit variance
-        banditVariance <- (1 - (chosenIndicator * kalmanGain)) * (banditVariance + (model$pars$zeta^2))
-        
-    }
+        if(any(choiceProb == 0)){
+            choiceProb[choiceProb == 0] <- realMin
+        }    
+    }, error = function(e){print(e)})
     
     return(list("negLL" = sum(-log(choiceProb)),
                 "choiceProb" = choiceProb))
