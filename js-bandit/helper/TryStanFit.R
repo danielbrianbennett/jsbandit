b7baafb5bb533c371d54483ab86023efed504225
Daniel Bennett <daniel.bennett@princeton.edu>
2018-05-01 15:44:38 -0400

    Update Stan models

--- js-bandit/helper/TryStanFit.R
+++ js-bandit/helper/TryStanFit.R
@@ -9,12 +9,43 @@ stanData <- ExtractDataForStan(here("raw_data","banditData_v2point2.Rdata"))
 stanData$V_init <- 100
 
 # list parameters to estimate in stan
-parameters <- c("beta",
+parameters <- c("mu_pr",
+                "sigma",
+                "beta",
                 "eta",
                 "B",
                 "j",
                 "k"
 ) 
+
+initVals = list(list(mu_pr = rep(0,5),
+                     sigma = rep(1,5),
+                     beta_pr =rep(0,stanData$N),
+                     eta_pr = rep(0,stanData$N),
+                     B_pr = rep(-10,stanData$N),
+                     j_pr = rep(-10,stanData$N),
+                     k_pr = rep(-10,stanData$N)),
+                list(mu_pr = rep(0,5),
+                     sigma = rep(1,5),
+                     beta_pr =rep(0,stanData$N),
+                     eta_pr = rep(0,stanData$N),
+                     B_pr = rep(-10,stanData$N),
+                     j_pr = rep(-10,stanData$N),
+                     k_pr = rep(-10,stanData$N)),
+                list(mu_pr = rep(0,5),
+                     sigma = rep(1,5),
+                     beta_pr =rep(0,stanData$N),
+                     eta_pr = rep(0,stanData$N),
+                     B_pr = rep(-10,stanData$N),
+                     j_pr = rep(-10,stanData$N),
+                     k_pr = rep(-10,stanData$N)),
+                list(mu_pr = rep(0,5),
+                     sigma = rep(1,5),
+                     beta_pr =rep(0,stanData$N),
+                     eta_pr = rep(0,stanData$N),
+                     B_pr = rep(-10,stanData$N),
+                     j_pr = rep(-10,stanData$N),
+                     k_pr = rep(-10,stanData$N)))
 
 # set parallel options
 rstan_options(auto_write = TRUE)
@@ -24,12 +55,13 @@ stanFile = here("helper","curious-bandits.stan")
 # Call stan 
 tic()
 samples <- stan(file = stanFile,   
-                data = stanData, 
+                data = stanData,
+                init = initVals,
                 pars=parameters,
-                iter=100, 
-                chains=2, 
+                iter=1000, 
+                chains=4, 
                 thin=1,
-                warmup = 50  # Default = iter/2
+                warmup = 500  # Default = iter/2
                 # seed = 123  # Setting seed; Default is random seed
 )
 toc()
--- js-bandit/helper/curious-bandits.stan
+++ js-bandit/helper/curious-bandits.stan
@@ -32,14 +32,14 @@ parameters {
 transformed parameters {
     
     // Subject-level parameters
-    vector<lower=0,upper=30>[N]         beta;
+    vector<lower=0,upper=10>[N]         beta;
     vector<lower=0,upper=1>[N]          eta;
     vector<lower=-100,upper=100>[N]     B;
     vector<lower=-10,upper=10>[N]       j;
     vector<lower=-10,upper=10>[N]       k;
 
     for (i in 1:N) {
-        beta[i]  = Phi_approx( mu_pr[1] + sigma[1] * beta_pr[i] ) * 30;
+        beta[i]  = Phi_approx( mu_pr[1] + sigma[1] * beta_pr[i] ) * 10;
         eta[i] = Phi_approx( mu_pr[2] + sigma[2] * eta_pr[i] );
         B[i] = Phi_approx( mu_pr[3] + sigma[3] * B_pr[i] ) * 100;
         j[i] = Phi_approx( mu_pr[4] + sigma[4] * j_pr[i] ) * 10;
@@ -51,7 +51,7 @@ model {
     
     // Generated data
     vector[4]           V;            // Q-values
-    vector[4]  choiceProb;        // choice probabilty container
+    //vector[4]  choiceProb;        // choice probabilty container
 
     // Group-level priors
     mu_pr ~ normal(0, 1);
@@ -64,7 +64,7 @@ model {
     j_pr ~ normal(0, 1);
     k_pr ~ normal(0, 1);
 
-    // Precompute values.
+    // Compute values.
     for (i in 1:T) {
         
         // Initialize/update value container at the beginning of each block 
@@ -76,13 +76,12 @@ model {
         if (whichFilled[i] != -1){
             V[whichFilled[i]] = V[whichFilled[i]] + ( B[participant_ix[i]] * ( block_ix[i] ^ j[participant_ix[i]] ) * ( changeLag[i] ^ k[participant_ix[i]] ) );
         }
-       
+ 
+         // likelihood with softmax
+        choice[i] ~ categorical(softmax(beta[participant_ix[i]] * V));
+        
         // update values based on observed outcome
         V[choice[i]] = V[choice[i]] + eta[participant_ix[i]] * ( outcome[i] - V[choice[i]]);
-        
-        // likelihood with stabilised softmax
-        choiceProb = exp((beta[participant_ix[i]] * V) - max(beta[participant_ix[i]] * V)) / sum(exp((beta[participant_ix[i]] * V) - max(beta[participant_ix[i]] * V)));
-        choice[i] ~ categorical(choiceProb);
         
     }
 
