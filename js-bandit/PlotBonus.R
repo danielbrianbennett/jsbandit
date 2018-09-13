2b3e174af14bec3c8320a77825564dd079843511
Daniel Bennett <daniel.bennett@princeton.edu>
2018-09-13 15:40:31 -0400

    Update bonus plot

--- js-bandit/PlotBonus.R
+++ js-bandit/PlotBonus.R
@@ -13,16 +13,17 @@ bonusSamples <- extract(samples, pars = c("mu_B","mu_j","mu_k","sigma_B","sigma_
 postMean <- lapply(bonusSamples, FUN = mean)
 
 # specify block and trial indices
-nTimePoints <- 10
+nTimePoints <- 15
 blockIndices <- c(rep(1, times = nTimePoints), rep(2, times = nTimePoints), rep(3, times = nTimePoints))
 trialIndices <- rep(1:nTimePoints, times = 3)
 
 # plot bonus size over time
 bonusSize <- mapply(FUN = function(B,j,k,blockIndex,trialIndex){return(B * (blockIndex ^ j) * (trialIndex ^ k))}, rep(postMean$mu_B, times = 3 * nTimePoints), rep(postMean$mu_j, times = 3 * nTimePoints), rep(postMean$mu_k,times = 3 * nTimePoints), blockIndices, trialIndices)
 bonusSize <- matrix(data = bonusSize, nrow = 3, ncol = nTimePoints, byrow = TRUE)
-plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = "Bonus size over time", ylim=c(0,25))
+plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = "Bonus size over time", ylim=c(-2,25))
 lines(1:nTimePoints, bonusSize[2,], type = "b", lwd = 2, col = "#979797")
 lines(1:nTimePoints, bonusSize[3,], type = "b", lwd = 2, col = "#D3D3D3")
+abline(h = 0, col="black", lty=3, lwd=2)
 legend(x = "topright", col = c("black","#979797","#D3D3D3"), lwd = c(2,2,2), legend = c("Block 1","Block 2", "Block 3"), bty = "n")
 
 ##### EXPERIMENT 2 #####
@@ -37,16 +38,17 @@ bonusSamples <- extract(samples, pars = c("mu_B","mu_j","mu_k","sigma_B","sigma_
 postMean <- lapply(bonusSamples, FUN = mean)
 
 # specify block and trial indices
-nTimePoints <- 10
+nTimePoints <- 15
 blockIndices <- c(rep(1, times = nTimePoints), rep(2, times = nTimePoints), rep(3, times = nTimePoints))
 trialIndices <- rep(1:nTimePoints, times = 3)
 
 # plot bonus size over time
 bonusSize <- mapply(FUN = function(B,j,k,blockIndex,trialIndex){return(B * (blockIndex ^ j) * (trialIndex ^ k))}, rep(postMean$mu_B, times = 3 * nTimePoints), rep(postMean$mu_j, times = 3 * nTimePoints), rep(postMean$mu_k,times = 3 * nTimePoints), blockIndices, trialIndices)
 bonusSize <- matrix(data = bonusSize, nrow = 3, ncol = nTimePoints, byrow = TRUE)
-plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = "Bonus size over time", col = "#6f5884", ylim=c(0,25))
+plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = "Bonus size over time", col = "#6f5884", ylim=c(-2,25))
 lines(1:nTimePoints, bonusSize[2,], type = "b", lwd = 2, col = "#9f7ebd")
 lines(1:nTimePoints, bonusSize[3,], type = "b", lwd = 2, col = "#cfbede")
+abline(h = 0, col="black", lty=3, lwd=2)
 legend(x = "topright", col = c("black","#979797","#D3D3D3"), lwd = c(2,2,2), legend = c("Block 1","Block 2", "Block 3"), bty = "n")
 
 ##### EXPERIMENT 3 #####
@@ -85,7 +87,7 @@ for (i in 1:3){
     # plot bonus size over time
     bonusSize <- mapply(FUN = function(B,j,k,blockIndex,trialIndex){return(B * (blockIndex ^ j) * (trialIndex ^ k))}, rep(B, times = 3 * nTimePoints), rep(j, times = 3 * nTimePoints), rep(k,times = 3 * nTimePoints), blockIndices, trialIndices)
     bonusSize <- matrix(data = bonusSize, nrow = 3, ncol = nTimePoints, byrow = TRUE)
-    plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = title, col = col[1], ylim=c(-30,60))
+    plot(1:nTimePoints, bonusSize[1,],  type = "b", lwd = 2, xlab = "Post-change lag", ylab = "Bonus size", main = title, col = col[1], ylim=c(-30,60), bty="n")
     lines(1:nTimePoints, bonusSize[2,], type = "b", lwd = 2, col = col[2])
     lines(1:nTimePoints, bonusSize[3,], type = "b", lwd = 2, col = col[3])
     abline(h = 0, col="black", lty=3, lwd=2)
